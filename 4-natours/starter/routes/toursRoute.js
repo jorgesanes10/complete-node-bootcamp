20654916ee56483f7752e6e527a2e5df7acbcea6
Jorge Sanes <jorgesanes10@gmail.com>
2024-12-20 14:49:31 -0500

    new classes

--- 4-natours/starter/routes/toursRoute.js
+++ 4-natours/starter/routes/toursRoute.js
@@ -5,15 +5,16 @@ const {
   createTour,
   updateTour,
   deleteTour,
+  getTourStats,
 } = require('../controllers/toursController');
 
 const router = express.Router();
 
 router.param('id', (req, res, next, value) => {
-  console.log(`Tour id is ${value}`);
   next();
 });
 
+router.route('/tour-stats').get(getTourStats);
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
